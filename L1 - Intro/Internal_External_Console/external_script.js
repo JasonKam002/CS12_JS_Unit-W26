3d11caa849250199b3e49fd4d23c50adb66c33cc
JasonKam002 <jasonkam1311@gmail.com>
2026-04-20 10:19:51 -0300

    update

---
+++ L1 - Intro/Internal_External_Console/external_script.js
@@ -0,0 +1,8 @@
+console.log("External script!");
+console.log(1);
+console.log(1+3);
+console.log("What","do","commas","do?");
+console.log("Does", "adding", "space", "matter?");
+console.log('Launch' + 'Code');
+console.log("LaunchCode was founded in", 2013);
+console.log('7'+'5 2', 3);
